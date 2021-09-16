8413f48ce6c49bd62772a0860ab03f1d2852ba58
mmoo55 <g.gerson51@gmail.com>
2021-09-15 20:56:09 -0400

    Creando plugin de AutoPlay

--- assets/MediaPlayer.js
+++ assets/MediaPlayer.js
@@ -1,11 +1,20 @@
 // la funcion prototipo que recibe el objeto config
 function MediaPlayer(config) {
-  this.media = config.el;
+    this.media = config.el;
+    this.plugins = config.plugins || [];  // || [] Da el valor inicial vacio
+
+    this._initPlugins();
+}
+
+MediaPlayer.prototype._initPlugins = function () {
+    this.plugins.forEach(plugin => {
+        plugin.run(this);
+    })
 }
 
 // prototype, agrega al objeto ya creado
 // le asicgnamos la función play
-MediaPlayer.prototype.play = function() {
+/* MediaPlayer.prototype.play = function() {
   if (this.media.paused) {  // Verificamos el estado del video para la opción play o pausa
     this.media.play();
   } else {
@@ -14,7 +23,31 @@ MediaPlayer.prototype.play = function() {
 
   // o podemos usar lo siguiente:
   // this.media.paused ? this.media.play() : this.media.pause()
-};
+}; */
+
+MediaPlayer.prototype.play = function () {
+    this.media.play();
+}
+
+MediaPlayer.prototype.pause = function () {
+    this.media.pause();
+}
+
+MediaPlayer.prototype.togglePlay = function () {
+    if (this.media.paused) {  // Verificamos el estado del video para la opción play o pausa
+        this.media.play();
+    } else {
+        this.media.pause();
+    }
+}
+
+MediaPlayer.prototype.mute = function () {
+    this.media.muted = true;
+}
+
+MediaPlayer.prototype.unmute = function () {
+    this.media.muted = false;
+}
 
 export default MediaPlayer;
 
--- assets/index.js
+++ assets/index.js
@@ -1,13 +1,14 @@
-import MediaPlayer from './MediaPlayer.js';
-
 // Para importar variables
 // import MediaPlayer, { foo } from './MediaPlayer.js';
+
+import MediaPlayer from './MediaPlayer.js';
+import AutoPlay from './plugins/AutoPlay.js';
 
 
 // querySelector, devuelve el primer elemento que coincida
 const video = document.querySelector('video')
 // Instanciamos al objeto MediaPlayer
-const player = new MediaPlayer({ el: video });
+const player = new MediaPlayer({ el: video, plugins: [new AutoPlay()] });
 
 const button = document.querySelector('button')
 button.onclick = () => player.play();

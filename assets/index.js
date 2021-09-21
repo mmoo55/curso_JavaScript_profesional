29904f9c63de4e84d8f896ca7f14b28e6e678c0b
mmoo55 <g.gerson51@gmail.com>
2021-09-21 14:26:46 -0400

    This

--- assets/index.js
+++ assets/index.js
@@ -8,7 +8,11 @@ import AutoPlay from './plugins/AutoPlay.js';
 // querySelector, devuelve el primer elemento que coincida
 const video = document.querySelector('video')
 // Instanciamos al objeto MediaPlayer
-const player = new MediaPlayer({ el: video, plugins: [new AutoPlay()] });
+const player = new MediaPlayer({
+    el: video, plugins: [
+        // new AutoPlay()
+    ]
+});
 
 // const button = document.querySelector('button')
 const button = document.querySelector('#playPause')

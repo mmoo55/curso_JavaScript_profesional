7060c009ac8db6ee182857153a79b0b75e8b4afb
mmoo55 <g.gerson51@gmail.com>
2021-09-30 19:27:28 -0400

    Tipos básicos

--- ejercicios/typescript/index.ts
+++ ejercicios/typescript/index.ts
@@ -1,8 +1,50 @@
-console.log('Hello, TypeScript');
+/* console.log('Hello, TypeScript');
 
-/* Se añade tipado a los parámetros */
+// Se añade tipado a los parámetros
 function add(a: number, b: number) {
   return a + b;
 }
 
-const sum = add(2, 3);
+const sum = add(2, 3); */
+
+
+// Boolean
+let muted: boolean = true;  // o simplemente let muted = true
+muted = false;
+
+// Números
+let age = 6;
+let numerador: number = 42;
+let denominador: number = age;
+let resultado = numerador / denominador;
+
+// String
+let nombre: string = 'Richard';
+let saludo = `Me llamo ${nombre}`;
+
+// Arreglos
+let people: string[] = [];
+people = ['Isabel', 'Nicole', 'Raul'];
+// people.push("9000")
+
+let peopleAndNumbers: Array<string | number> = [];      /* Array de dos tipos diferentes */
+peopleAndNumbers.push('Ricardo');
+peopleAndNumbers.push(9001);
+
+// Enum
+enum Color {    /* Conjunto de valores que se define, no se puede añadir ni quitar */
+  Rojo = 'Rojo',
+  Verde = 'Verde',
+  Azul = 'Azul',
+  Amarillo = 'Amarillo',
+}
+
+let colorFavorito: Color = Color.Amarillo;
+console.log(`Mi color favorito es ${colorFavorito}`);
+
+// Any
+let comodin: any = 'Joker';
+comodin = { type: 'Wildcard' };
+
+// Object
+let someObject: object = { type: 'Wildcard' };

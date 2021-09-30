64ead17a22234d50f5512dc6f7d29cd19328b4b4
mmoo55 <g.gerson51@gmail.com>
2021-09-30 19:46:01 -0400

    Funciones en TypeScript

--- ejercicios/typescript/index.ts
+++ ejercicios/typescript/index.ts
@@ -1,8 +1,8 @@
-/* console.log('Hello, TypeScript');
+console.log('Hello, TypeScript');
 
-// Se añade tipado a los parámetros
-function add(a: number, b: number) {
-  return a + b;
+/* Se añade tipado a los parámetros */
+/* function add(a: number, b: number) {
+    return a + b;
 }
 
 const sum = add(2, 3); */
@@ -33,10 +33,10 @@ peopleAndNumbers.push(9001);
 
 // Enum
 enum Color {    /* Conjunto de valores que se define, no se puede añadir ni quitar */
-  Rojo = 'Rojo',
-  Verde = 'Verde',
-  Azul = 'Azul',
-  Amarillo = 'Amarillo',
+    Rojo = 'Rojo',
+    Verde = 'Verde',
+    Azul = 'Azul',
+    Amarillo = 'Amarillo',
 }
 
 let colorFavorito: Color = Color.Amarillo;
@@ -48,3 +48,36 @@ comodin = { type: 'Wildcard' };
 
 // Object
 let someObject: object = { type: 'Wildcard' };
+
+
+// Funciones
+function add(a: number, b: number): number {    /* El ultimo valor nos dice que valor devolverá la función */
+    return a + b;
+}
+
+const sum = add(4, 6);
+
+                                /* Representa lo que devuelve la función, en este caso toma un número (number) y devuelve un número => number, lo cual indica que el valor de regreso es una función */
+function createAdder(a: number): (number) => number {
+    return function (b: number) {
+        return b + a;
+    };
+}
+
+const addFour = createAdder(4);
+const fourPlus6 = addFour(6);
+
+/* Se agrega el signo de interrogacion para que un parámetro sea opcional que quedaria como undefined */
+function fullName(firstName: string, lastName?: string): string {
+    return `${firstName} ${lastName}`;
+}
+
+const richard = fullName('Richard');
+
+/* Si queremos que tenga un valor por omisión lo agregamos despues del tipado, de este modo sino mandamos un argumento tomará por el valor por omisión */
+function fullName1(firstName: string, lastName: string = 'Smith'): string {
+    return `${firstName} ${lastName}`;
+}
+
+const richard1 = fullName1('Agente');
+console.log(richard1);

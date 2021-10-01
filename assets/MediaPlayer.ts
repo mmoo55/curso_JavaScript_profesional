a8cd0da252dc10705b993d227966f2ebc2463062
mmoo55 <g.gerson51@gmail.com>
2021-10-01 02:31:21 -0400

    Implementación de plugin de Ads: Desplegando en pantalla

--- assets/MediaPlayer.ts
+++ assets/MediaPlayer.ts
@@ -1,12 +1,22 @@
 class MediaPlayer {
   media: HTMLMediaElement;
   plugins: Array<any>;
+  container: HTMLElement;
 
   constructor(config) {
     this.media = config.el;
     this.plugins = config.plugins || [];
+    this.initPlayer();
     this.initPlugins();
   }
+
+  initPlayer() {  /* Inicializamos el player para crear el contenedor de forma dinámica y que quede dentro el video */
+    this.container = document.createElement('div');
+    this.container.style.position = 'relative';    /* Para poner los ads usaremos posición absoluta para poder ponerlo en la parte de abajo y para eso necesitamos poner posición relativa en el contenedor */
+    this.media.parentNode.insertBefore(this.container, this.media);   /* Ubicamos el contenedor como pariente de media */
+    this.container.appendChild(this.media);   /* Ya que esté pariente, tomamos el media y lo ponemos dentro */
+  }
+
   private initPlugins() {
     this.plugins.forEach(plugin => {
       plugin.run(this);
--- assets/plugins/Ads/index.ts
+++ assets/plugins/Ads/index.ts
@@ -6,14 +6,17 @@ class AdsPlugin {
   private player: MediaPlayer;
   private media: HTMLMediaElement;
   private currentAd: Ad;
+  private adsContainer: HTMLElement;
 
   constructor() {
     this.ads = Ads.getInstance();   /* Es singleton, por lo tanto no hay constructor público, para eso utilizamos el método getInstance() */
+    this.adsContainer = document.createElement('div');    /* Contenedor con todos los ads que vamos a añadir y ese contenedor a su vez estará dentro del contenedor del MediaPlayer */
     this.handleTimeUpdate = this.handleTimeUpdate.bind(this);   /* Ya que utiliza otro this, lo guardamos con bind() */
   }
 
   run(player: MediaPlayer) {    /* Todos los plugins tienen un método público llamado run */
     this.player = player;
+    this.player.container.appendChild(this.adsContainer);
     this.media = this.player.media;
     this.media.addEventListener('timeupdate', this.handleTimeUpdate);   /* timeupdate; cada vez que pasa un tiempo anuncia que cambio el tiempo, se lo utiliza para realizar diferentes acciones */
   }
@@ -32,7 +35,24 @@ class AdsPlugin {
 
     const ad = this.ads.getAd();
     this.currentAd = ad;
-    console.log(this.currentAd);
+    // console.log(this.currentAd);
+
+    this.adsContainer.innerHTML = `
+      <div class="ads">
+        <a class="ads__link" href="${this.currentAd.url}" target="_blank">
+          <img class="ads__img" src="${this.currentAd.imageUrl}" />
+          <div class="ads__info">
+            <h5 class="ads__title">${this.currentAd.title}</h5>
+            <p class="ads__body">${this.currentAd.body}</p>
+          </div>
+        </a>
+      </div>
+    `;    /* Rellenamos el div vacio que creamos para los ads */
+
+    setTimeout(() => {
+      this.currentAd = null;    /* En cierto tiempo toma el currentAd y lo vuelve null */
+      this.adsContainer.innerHTML = '';   /* El HTML interno lo vacia de adsContainer */
+    }, 10000);
   }
 }
 
